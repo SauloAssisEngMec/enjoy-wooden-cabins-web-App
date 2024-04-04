cf269558c868105fd408c3c857e2ab271cd4ccde
saulo <sauloassis4123@gmail.com>
2024-04-04 12:14:17 -0300

    style: setup globla styles and building some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,41 @@
+import styled from "styled-components"
+import GlobalStyles from "./styles/GlobalStyles"
+import Heading from "./components/Heading"
+import Button from "./components/Button"
+import Input from "./components/Input"
+import Row from "./components/Row"
+
+styled
+
 function App() {
   return (
-    <div>
-      enjoy-w-c
-    </div>
+    <>
+    <GlobalStyles/>
+    <Row >
+    <Row type='horizontal'>
+      <Heading as="h1">
+        Enjoy wooden cabins
+      </Heading>
+      <div>
+        <Heading as="h2">
+          register
+        </Heading>
+
+        <Button>Check in</Button>
+        <Button variation='secondary' size='small' >Check out</Button>
+      </div>
+    </Row>
+    <Row >
+      <Heading as="h3">
+        Form
+      </Heading>
+      <form>
+        <Input type="number"></Input>
+        <Input type="number"></Input>
+      </form>
+    </Row>
+    </Row>
+    </>
   )
 }
 
