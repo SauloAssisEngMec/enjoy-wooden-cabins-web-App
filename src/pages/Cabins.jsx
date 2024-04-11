b507d98d8538954a19f29b08698210f3231ba928
saulo <sauloassis4123@gmail.com>
2024-04-11 19:12:10 -0300

    feat: Connecting frontend to supabase, our backend chosen

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,4 +1,10 @@
+import { useEffect } from "react"
+import { getCabins } from "../services/apiCabins"
+
 function Cabins() {
+    useEffect(() => {
+   getCabins().then((data) => console.log(data))
+    },[])
     return (
         <div>
             Cabins
